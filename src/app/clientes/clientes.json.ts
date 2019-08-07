d00d09819c27b5789f913f4327fbda76242fb161
jeissonmgz <jeissonmgz@gmail.com>
2019-08-06 22:31:18 -0500

    Creación del componente clientes

---
+++ src/app/clientes/clientes.json.ts
@@ -0,0 +1,15 @@
+import {Cliente} from './cliente';
+
+export const CLIENTES: Cliente[] = [
+  {id: 1, nombre: 'Natalia', apellido: 'Sierra', email: 'nsierra@gmail.com', createAt:'2018-08-06'},
+  {id: 2, nombre: 'Sandra', apellido: 'Galvis', email: 'sgalvis@gmail.com', createAt:'2018-09-06'},
+  {id: 3, nombre: 'Patricia', apellido: 'Alvis', email: 'palvis@gmail.com', createAt:'2019-01-06'},
+  {id: 4, nombre: 'Jose', apellido: 'Silva', email: 'jsilva@gmail.com', createAt:'2019-02-06'},
+  {id: 5, nombre: 'Edgar', apellido: 'Sucre', email: 'esucre@gmail.com', createAt:'2019-03-06'},
+  {id: 6, nombre: 'Camila', apellido: 'Niño', email: 'cninio@gmail.com', createAt:'2019-04-06'},
+  {id: 7, nombre: 'Yudy', apellido: 'Soler', email: 'ysoler@gmail.com', createAt:'2019-04-16'},
+  {id: 8, nombre: 'Cristian', apellido: 'Gutierrez', email: 'cguti@gmail.com', createAt:'2019-05-06'},
+  {id: 9, nombre: 'Paola', apellido: 'Castro', email: 'pcastro@gmail.com', createAt:'2019-06-06'},
+  {id: 10, nombre: 'Lina', apellido: 'Gomez', email: 'linagomez@gmail.com', createAt:'2019-07-02'},
+  {id: 11, nombre: 'Jeisson', apellido: 'Malaver', email: 'jeissonmgz@gmail.com', createAt:'2019-08-06'}
+];
