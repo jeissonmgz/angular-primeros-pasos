b3feb3405a710ba3570e1db13a7a60d48488ab32
jeissonmgz <jeissonmgz@gmail.com>
2019-08-06 22:39:43 -0500

    Configurando servicio y agregar al app.module

--- src/app/clientes/cliente.service.ts
+++ src/app/clientes/cliente.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
+import {Cliente} from './cliente';
+import {CLIENTES} from './clientes.json';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable()
 export class ClienteService {
 
   constructor() { }
+
+  getClientes(): Cliente[] {
+    return CLIENTES;
+  }
+
 }
--- src/app/clientes/clientes.json.ts
+++ src/app/clientes/clientes.json.ts
@@ -4,7 +4,7 @@ export const CLIENTES: Cliente[] = [
   {id: 1, nombre: 'Natalia', apellido: 'Sierra', email: 'nsierra@gmail.com', createAt:'2018-08-06'},
   {id: 2, nombre: 'Sandra', apellido: 'Galvis', email: 'sgalvis@gmail.com', createAt:'2018-09-06'},
   {id: 3, nombre: 'Patricia', apellido: 'Alvis', email: 'palvis@gmail.com', createAt:'2019-01-06'},
-  {id: 4, nombre: 'Jose', apellido: 'Silva', email: 'jsilva@gmail.com', createAt:'2019-02-06'},
+  {id: 4, nombre: 'Alberto', apellido: 'Silva', email: 'jsilva@gmail.com', createAt:'2019-02-06'},
   {id: 5, nombre: 'Edgar', apellido: 'Sucre', email: 'esucre@gmail.com', createAt:'2019-03-06'},
   {id: 6, nombre: 'Camila', apellido: 'Niño', email: 'cninio@gmail.com', createAt:'2019-04-06'},
   {id: 7, nombre: 'Yudy', apellido: 'Soler', email: 'ysoler@gmail.com', createAt:'2019-04-16'},
