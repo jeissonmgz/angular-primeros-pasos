e771b0811f3443828445aabafd6037a3677ce3a1
jeissonmgz <jeissonmgz@gmail.com>
2019-08-07 21:18:41 -0500

    Activar servico reactivo

--- src/app/clientes/cliente.service.ts
+++ src/app/clientes/cliente.service.ts
@@ -1,14 +1,15 @@
 import { Injectable } from '@angular/core';
 import {Cliente} from './cliente';
 import {CLIENTES} from './clientes.json';
+import {of, Observable} from 'rxjs';
 
 @Injectable()
 export class ClienteService {
 
   constructor() { }
 
-  getClientes(): Cliente[] {
-    return CLIENTES;
+  getClientes(): Observable<Cliente[]> {
+    return of(CLIENTES);
   }
 
 }
